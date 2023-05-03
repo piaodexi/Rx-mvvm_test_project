eb30c8253bde4f2e4f3003596d538378669d72e2
dexi <piaodexi94@gmail.com>
2023-05-04 00:27:03 +0900

    [rxswift mvvm 구조에서 rxswift input,output mvvm 패턴으로 변경]
    - 주석정리
    - 코드정리

--- MyMovieApp/MyMovieApp/View/BoxOfficeMainViewController.swift
+++ MyMovieApp/MyMovieApp/View/BoxOfficeMainViewController.swift
@@ -9,39 +9,11 @@ import Foundation
 import UIKit
 import RxSwift
 import RxCocoa
-//TODO 일별?박스오피스 추가
-//TODO 영화 검색 추가
-//TODO 테이블 셀에서 영화 터치시 영화 상세뷰 추가
-//TODO 처음 테이블리스트 가져올때 인디케이터 적용, 주간,주말,날짜선택시 인디게이터 적용
-//TODO 앱이 처음 시작될때 주간 주말 리스트를 가여오는데 이때 날짜값을 하드코딩해놈. 오늘날짜에서 api를 호출하기에 유효한 날짜로 치환하는 로직작성후 변경작업필요
-//TODO 캘린더에서 날짜 선택에서 날짜를 받아올때 일주일 전 주말,주간 날이 아니면 주간,주말 리스트를 받아오는 api를 실행했을때 데이터를 못받아옴.
-//이부분을 input (캘린더에서 선택한날짜) output (캘린더에서 선택한날짜가 유효한 날짜이면 api호출) 패턴으로 작성가능할듯 싶은데... ...
-//하고는 싶은데...안되면 그냥 유효성 처리 하는 로직 작성해서 처리해보자 ..
-//TODO 하드코딩한 파라미터에서 로직 처리하도록 변경필요
-/**
- 1 . public과 private의 차이점.
- -> 접근제어자의 역활임. public은 모든 파일에서 접근가능 , private는선언한 범위내에서만 사용가능해서 영향도가 해당범위에만 미치지만
- 이로 인해 다른코드에서 public을 접근해서 쓸때 만약 값이 의도치 않는 값으로 변경될때에 대한 영향도가 커질 수 있는 차이가 있음.
- 
- 2. BehaviorSubject와 PublishSubject의 차이점
- -> publishsubject는 초깃값이 없고 구독될때 새로운 이벤트만 전달함. behaviorsubject는 초깃값을 가지며 구독될때 초깃값을 전달하고 이벤트가 오면 이벤트 모두 전달함 . 초깃값이 있다보니 항상값을 방출 할 수 있음
- 
- 3. 캘린더 컨트롤러에서 박스오피스메인뷰컨트롤러로 데이터를 넘겨줄때 캘린더 뷰모델이 아닌 위클리박스오피스모델을 사용해서 어떻게 처리할것인지.
- -> 위클리박스오피스모델에서 publishSubjectfh 옵저버블하나 만들어서 처리하는것으로 함. 뷰컨트롤러간에 다이렉트로 넘기는 방법에 대해서도 고민을 해봣지만
- 지금은 작성한게 없지만 추후 받아오는 날짜에 대한 유효성 처리가 필요하므로 그에 대한 비즈니스 로직을 작성할려면 뷰모델이 필요할테니 뷰모델을 가지는 방향으로
- 위클리박스오피스에서 비헤비어서브젝트로 날짜데이터를 체킹할 수 있게 옵저버블을 만들고 초깃값으로 현재날짜를 셋팅해서 뷰가 처음 켜지는 순간엔 초깃값을 쓰고 추후에 날짜데이터가 변경이 되면 변경이 되는 값으로 쓰는
- 방향으로 가도 ㄱㅊ을듯
- 4. setTitle함수의 이름에 대한 고민과 이것을 뷰모델로 빼서 처리할 방법
- -> 이름은 getToDay로 변경하고 위클리박스오피스뷰모델에 날짜 스트링반환함수작성하고 박스오피스메인뷰컨트롤러에서 가져다 씀
- 5. viewdidload같이 이미 구현된 함수를 오버라이드 해서 쓸 경우 super 쓰고 안쓰고에 대한 차이점
- super.viewDidload()를 호출안해도 viewDidLoad()는 호출됨.그러나 부모 클래스 유아이뷰컨트롤러에서 필요한 초기화 작업이 진행이 안될 수도 있음.
- 6. 코드 줄맞춤. rxswift는 한줄에 안씀
- */
+
 class BoxOfficeListTableViewController: UIViewController{
     
     @IBOutlet private var today: UINavigationItem!
     @IBOutlet private var tableView: UITableView!
-    
     @IBOutlet private weak var segmentedControl: UISegmentedControl!
     
     private let weeklyBoxOfficeListVM = WeeklyBoxOfficeListVM()
@@ -50,102 +22,45 @@ class BoxOfficeListTableViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = false
-        self.today.title = try!weeklyBoxOfficeListVM.item.value().selectDate
-        self.bind(targetDt: "20230415", weekGb: "0")
+        self.titleDataBind()
+        self.tableViewDataBind()
     }
-    
+
+    //세그웨이처리
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let calenderVC = segue.destination as? CalenderViewController else {
             fatalError("..")
         }
-        calenderVC.weaklyBoxOfficeVM.item
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: {  calender in
-                self.today.title = calender.selectDate
-                self.bind(targetDt: "20230415", weekGb: "0")
-                //self.bind(targetDt: self.today.title!.components(separatedBy: ["-"]).joined(), weekGb: "0")
-            })
-            .disposed(by: disposeBag)
+        calenderVC.weaklyBoxOfficeVM = self.weeklyBoxOfficeListVM
     }
 
-    @IBAction private func selectWeekedWeekday(_ sender: Any) {
+    //세그먼트제어
+    @IBAction func selectWeekedWeekday(_ sender: Any) {
         if (sender as AnyObject).selectedSegmentIndex == 0 {
-            weeklyBoxOfficeListVM.dataSetUp(targetDt: self.today.title!.components(separatedBy: ["-"]).joined(), weekGb: "0")
+            self.weeklyBoxOfficeListVM.targetDateInput.onNext(self.today.title!.components(separatedBy: ["-"]).joined())
+            self.weeklyBoxOfficeListVM.weekGbInput.onNext("0")
         } else {
-            print("check")
-            weeklyBoxOfficeListVM.dataSetUp(targetDt: self.today.title!.components(separatedBy: ["-"]).joined(), weekGb: "1")
+            self.weeklyBoxOfficeListVM.targetDateInput.onNext(self.today.title!.components(separatedBy: ["-"]).joined())
+            self.weeklyBoxOfficeListVM.weekGbInput.onNext("1")
         }
     }
-
-    private func bind(targetDt: String, weekGb: String) {
-        /**
-         RxCocoa를 사용하여 UITableViewCell을 구성하는 4가지 방법
-         1 rx.items(dataSource:protocol<RxTableViewDataSourceType, UITableViewDataSource>)
-         ->  RxSwift에서 제공하는 RxTableViewDataSourceType 프로토콜과 UITableViewDataSource 프로토콜을 채택한 DataSource를 이용하여 TableView의 데이터를 설정하는 Operator. 이 Operator를 쓰면 TableView의 Delegate와 DataSource를 분리하여 코드를 작성할 수 있음
-         2 rx.items(cellIdentifier:String)
-         -> tableView에서 사용할 Cell의 Identifier를 지정하여, TableView의 데이터를 설정하는 Operator. 이 Operator를 쓰면 TableView의 DataSource를 구현하지 않아도 됨
-         3 rx.items(cellIdentifier:String:Cell.Type:_:)
-         -> tableView에서 사용할 Cell의 Identifier와 Cell 클래스를 지정하여, TableView의 데이터를 설정하는 Operator. 이 Operator를 이용하면 TableView의 DataSource를 구현하지 않아도되고 셀 커스텀도 가능
-         4 rx.items(_:_:)
-         -> tableView에서 사용할 Section과 Cell을 커스텀하여 지정하여, TableView의 데이터를 설정하는 Operator. 이 Operator를 이용하면 TableView의 DataSource를 구현하지 않아도 되고 Section을 사용하여 TableView의 데이터를 구성할 수 있음
-         */
-        /**
-         아래 코드가 가독성이 안좋음
-         왜냐하면 줄바꿈을 할 수 있음에도 불구하고 한줄에 작성을 해놓았기 때문에
-         weeklyBoxOfficeListVM.list
-             .bind(to: tableView.rx.items(cellIdentifier: "BoxOfficeListCell")) { (index, item: WeeklyBoxOfficeList, cell: BoxOfficeListCell) in
-             cell.setData(item)
-         }.disposed(by: disposeBag)
-         */
-//        weeklyBoxOfficeListVM.list
-//            .bind(to: tableView.rx.items(cellIdentifier: "BoxOfficeListCell")) { (index, item: WeeklyBoxOfficeList, cell: BoxOfficeListCell) in
-//            cell.setData(item)
-//        }.disposed(by: disposeBag)
-        //줄바꿈과 더불어 rxcocoa를 사용하여 uitableviewcell을 구성하는 4가지 방법중 하나로 변경해서 작성함.
-        weeklyBoxOfficeListVM.list
-            .asObservable()
-            .bind(to: tableView.rx.items(
-                cellIdentifier: "BoxOfficeListCell",
-                cellType: BoxOfficeListCell.self)) { index, item, cell in
-                    cell.setData(item)
-            }.disposed(by: disposeBag)
-//
-        weeklyBoxOfficeListVM.dataSetUp(targetDt: targetDt, weekGb: weekGb)
+    
+    //tableview에 쓸 데이터를 바인딩함.
+    private func tableViewDataBind() {
+        self.weeklyBoxOfficeListVM.targetDateInput.onNext("20230101")
+        self.weeklyBoxOfficeListVM.weekGbInput.onNext("0")
+        self.weeklyBoxOfficeListVM.listOutput
+            .bind(to: tableView.rx.items(cellIdentifier: "BoxOfficeListCell")) { (index, item: WeeklyBoxOfficeList, cell: BoxOfficeListCell) in
+                cell.setData(item)
+            }
+            .disposed(by: disposeBag)
     }
     
-//-------------input output 패턴 ---------------------
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.navigationController?.navigationBar.prefersLargeTitles = false
-//        weeklyBoxOfficeListVM.titleOutput
-//            .observe(on: MainScheduler.instance)
-//            .bind(to: today.rx.title)
-//            .disposed(by: disposeBag)
-//
-//        weeklyBoxOfficeListVM.selectDateInput.onNext(Date())
-//        self.bind()
-//    }
-//
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let calenderVC = segue.destination as? CalenderViewController else {
-//            fatalError("..")
-//        }
-//        calenderVC.weaklyBoxOfficeVM = weeklyBoxOfficeListVM
-//    }
-//
-//    private func bind() {
-//        segmentedControl.rx.selectedSegmentIndex
-//            .map { $0 == 0 ? ("20230415", "0") : ("20230101", "1") }
-//            .bind {  targetDt, weekGb in
-//                self.weeklyBoxOfficeListVM.targetDateInput.onNext(targetDt)
-//                self.weeklyBoxOfficeListVM.weekGbInput.onNext(weekGb)
-//            }
-//            .disposed(by: disposeBag)
-//
-//        weeklyBoxOfficeListVM.listOutput
-//            .bind(to: tableView.rx.items(cellIdentifier: "BoxOfficeListCell")) { (index, item: WeeklyBoxOfficeList, cell: BoxOfficeListCell) in
-//                cell.setData(item)
-//            }
-//            .disposed(by: disposeBag)
-//    }
+    //상단 네비 타이틀의 날짜데이터를 바인딩함.
+    private func titleDataBind() {
+        self.weeklyBoxOfficeListVM.selectDateInput.onNext(Date())
+        self.weeklyBoxOfficeListVM.titleOutput
+            .bind(to: today.rx.title)
+            .disposed(by: disposeBag)
+    }
 }
--- MyMovieApp/MyMovieApp/ViewModel/WeeklyBoxOfficeVM.swift
+++ MyMovieApp/MyMovieApp/ViewModel/WeeklyBoxOfficeVM.swift
@@ -9,63 +9,45 @@ import RxSwift
 class WeeklyBoxOfficeListVM {
     //Disposable 객체들을 담을 DisposeBag 객체를 생성
     let disposeBag = DisposeBag()
-    //새로운 구독자가 등록되면 발행된 이후의 값을 발행하는 PublishSubject를 사용하여 WeeklyBoxOfficeList[] 타입의 객체를 옵져버블인 list을 생성
-    let list = PublishSubject<[WeeklyBoxOfficeList]>()
-    //갤런디버큐컨트롤러에서 쓸 옵져버블 item 객체 생성
-    //PublishSubject가 아닌 BehaviorSubject를 사용하여 처리
-    //let item = PublishSubject<Calender>()//DateFormatter().string(from: Date())
-    let item = BehaviorSubject<Calender>(value: Calender(selectDate: ""))
-   //오늘날짜로 초기값을 잡아줌.
+    // Input
+    let selectDateInput = PublishSubject<Date>()
+    let targetDateInput = PublishSubject<String>()
+    let weekGbInput = PublishSubject<String>()
+
+    // Output
+    let titleOutput = BehaviorSubject<String>(value: "")
+    let listOutput = BehaviorSubject<[WeeklyBoxOfficeList]>(value: [])
+
     init() {
+        /**
+         DateFormatter 객체를 생성, 속성을 "yyyy-MM-dd"로 셋팅
+         캘린더컨트롤뷰에서 선택된 날짜를 방출하는 selectDateInput 옵저버블을 구독
+         map 연산자를 사용하여 선택된 날짜를 문자열로 변환
+         변환된 문자열을 titleOutput 옵저버블에 바인딩
+         */
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
-        let dateString = dateFormatter.string(from: Date())
-        item.onNext(Calender(selectDate: dateString))
-    }
-    
-    
-    //주간 주말리스트 처리
-    func dataSetUp(targetDt: String, weekGb: String) {
-        //APIService에서 제공하는 loadBoxOffice 메소드를 호출하여, Observable 객체를 생성
-        APIService.loadBoxOffice(targetDt: targetDt, weekGb: weekGb)
-        //MainScheduler를 사용하여 subscribe 이벤트가 메인 스레드에서 실행되도록 설정
-            .observe(on: MainScheduler.instance)
-        //subscribe 메소드를 호출하여 옵저버를 등록하고 onNext를 통해 WeeklyBoxOfficeList 배열을 전달하고, BehaviorSubject의 list에 저장. Disposable 객체를 disposeBag에 추가하여 Observable이 해지될 때 메모리 해제 수행
-            .subscribe(onNext: {weeklyBoxOfficeList in
-                self.list.onNext(weeklyBoxOfficeList)
-            }).disposed(by: disposeBag)
-    }
-    /**
-     
-     */
-//-------------input output 패턴 ---------------------
-//    // Input
-//    let selectDateInput = PublishSubject<Date>()
-//    let targetDateInput = PublishSubject<String>()
-//    let weekGbInput = PublishSubject<String>()
-//
-//    // Output
-//    let titleOutput = BehaviorSubject<String>(value: "")
-//    let listOutput = BehaviorSubject<[WeeklyBoxOfficeList]>(value: [])
-//
-//    init() {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd"
-//        selectDateInput
-//            .map { dateFormatter.string(from: $0) }
-//            .bind(to: titleOutput)
-//            .disposed(by: disposeBag)
-//
+        selectDateInput
+            .map { dateFormatter.string(from: $0) }
+            .bind(to: titleOutput)
+            .disposed(by: disposeBag)
 
-    //   //combineLatest() 메소드는 두 Observable에서 최신 값을 가져와서 튜플로 반환
-//        Observable.combineLatest(targetDateInput, weekGbInput)
-        //APIService.loadBoxOffice() 메소드가 반환하는 새로운 Observable로 전환,flatMapLatest()는 새로운 Observable을 반환하므로 새로운 값을 방출하면 기존의 Observable을 취소하고 새로운 Observable에 대한 구독을 시작
-//            .flatMapLatest { (targetDt, weekGb) in
-//                APIService.loadBoxOffice(targetDt: targetDt, weekGb: weekGb)
-//            }
-//            .observe(on: MainScheduler.instance)
-    //          // listOutput Observable과 UITableView에 데이터를 바인딩
-    //            .bind(to: listOutput)
-//            .disposed(by: disposeBag)
-//    }
+       /**
+        combineLatest를 사용하여 argetDateInput과 weekGbInput 두 개의 Observable을 합쳐서 새로운 이벤트를 방출하는 Observable을 만들어줌
+        flatMapLatest 연산자를 사용하여 loadBoxOffice 메소드가 반환하는 Observable을 구독하고 새로운 이벤트를 방출할 때마다 이전에 구독한 내부 Observable을 버리고새로운 내부 Observable을 구독
+        bind(to:) 연산자를 이용하여 최종적으로 listOutput에 값을 바인딩
+        마지막으로 disposed으로 메모리 해제함.
+        */
+        Observable.combineLatest(targetDateInput, weekGbInput)
+            .flatMapLatest { (targetDt, weekGb) in
+                APIService.loadBoxOffice(targetDt: targetDt, weekGb: weekGb)
+            }
+            .bind(to: listOutput)
+            .disposed(by: disposeBag)
+    }
+    
+    //입력된날짜가 7일전인지 아닌지 판별
+    func checkDate(inputDate: Date) -> Bool {
+        return inputDate >= Calendar.current.date(byAdding: .day, value: -7, to: Date()) ?? Date() ? true : false
+    }
 }
